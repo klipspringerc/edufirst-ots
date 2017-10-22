3e555665c1cfbe9a3236935240929bf591cd3d22
klipspringersc <kevincui1997@gmail.com>
2017-10-22 14:11:40 +0800

    Changes to adhere to API spec Add API spec file

--- backend/users/views.py
+++ backend/users/views.py
@@ -16,16 +16,18 @@ def signup_view(request):
         password_verify = request.POST['password2']
         try:
             user = User.objects.get(username=username)
-            return render(request, 'users/signupd.html', {'errormsg': "Username already taken"})
+            return JsonResponse({"status": "failure", "message": "username already taken"}, status=409)
         except User.DoesNotExist:
             if password == password_verify:
                 user = User.objects.create_user(username=username, password=password)
                 account = Account(user=user, certificate="None")
+                if 'email' in request.POST:
+                    account.email = request.POST['email']
                 account.save()
                 login(request, user)
-                return HttpResponse("signup success", status=200)
+                return JsonResponse({"status": "success", "message": "account created"}, status=201)
             else:
-                return render(request, 'users/signupd.html', {'errormsg': "Passwords didn\'t match"})
+                return JsonResponse({"status": "failure", "message": "password does not match"}, status=406)
     else:
         return render(request, 'users/signupd.html')
 
@@ -37,16 +39,16 @@ def login_view(request):
         user = authenticate(username=username, password=password)
         if user is not None:
             login(request, user)
-            return HttpResponse("login success", status=200)
+            return JsonResponse({"status": "success", "message": "login successful"}, status=200)
         else:
-            return render(request, 'users/logind.html', {'errormsg': "incorrect password"})
+            return JsonResponse({"status": "failure", "message": "username not exist or incorrect password"}, status=406)
     else:
         return render(request, 'users/logind.html')
 
 
 def logout_view(request):
     logout(request)
-    return HttpResponse("logout success", status=200)
+    return JsonResponse({"status": "success", "message": "logout successful"}, status=200)
 
 
 def user_posts_debug_view(request, user_id):
