f65d1fe873362874b937cf6f0462ff370ce987cb
Chen-Hailin <chenhailin2@gmail.com>
2017-10-23 07:56:06 +0800

    chl

--- frontend/src/components/AnswersArea.js
+++ frontend/src/components/AnswersArea.js
@@ -1,26 +1,57 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import {Card, CardHeader, CardMedia, CardText, CardActions} from 'material-ui/Card';
+import {MenuItem, Nav, Navbar, NavDropdown, NavItem, Row, Col} from 'react-bootstrap';
+import {IconButton, FlatButton} from 'material-ui';
+import favIcon from 'material-ui/svg-icons/action/done';
+import {connect} from 'react-redux';
+import {Link} from 'react-router-dom';
+import {putLike} from '../actions/likes';
 
-const AnswersArea = ({answers}) => (
+const AnswersArea = ({answers, handlePutLike}) => (
     <div>
       {answers.map(answer => (
-          <div key={answer.id}>
-            <div className="user">{answer.author.username}</div>
-            <div className="certificate">{answer.author.certificate}</div>
-            <div className="answer-detail">{answer.body}</div>
-            <div className="horizontal-rule"/>
-            <div className="likes">{answer.votes_total}</div>
-
-            {/*<CommentArea likes={answer.votes_total}*/}
-            {/*comments={answer.comments}*/}
-            {/*answerId={answer.id}/>*/}
-          </div>
+          <Row className="col-md-10">
+            <Col xsOffset={4} className="col-centered" >
+              <Card style={{width: '100%', height: '30%'}}>
+                <CardHeader 
+                  title={answer.author.username}
+                  subtitle={(answer.author.certificate)? answer.author.certificate : 'No certificate'}
+                  actAsExpander={true}
+                  showExpandableButton={true}/>
+                <CardText expandable={true}>
+                    <div className="body">{answer.body}</div>
+                </CardText>
+                <CardActions>
+                  <FlatButton label={answer.votes_total} onClick={(answer)=>(answer.votes_total += 1)}  />
+                </CardActions>
+              </Card>
+            </Col>
+            <div style={{height:20}}/>
+          </Row>
       ))}
     </div>
 );
 
 AnswersArea.propTypes = {
   answers: PropTypes.array.isRequired,
+  handlePutLike: PropTypes.func.isRequired,
 };
-
-export default AnswersArea;
+const mapDispatchToProps = (dispatch) => ({
+    handlePutLike: answerId => dispatch(putLike(answerId)),
+})
+export default connect(null, mapDispatchToProps)(AnswersArea);
+//export default AnswersArea;
+/*
+<div key={answer.id}>
+            <div className="user">{answer.author.username}</div>
+            <div className="certificate">{answer.author.certificate}</div>
+            <div className="answer-detail">{answer.body}</div>
+            <div className="horizontal-rule"/>
+            <div className="likes">{answer.votes_total}</div>
+            <div style={{height:20}}/>
+            {/*<CommentArea likes={answer.votes_total}* /}
+            {/*comments={answer.comments}* /}
+            {/*answerId={answer.id}/>* /}
+          </div>
+*/
--- frontend/src/components/QuestionDetail.js
+++ frontend/src/components/QuestionDetail.js
@@ -1,19 +1,38 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import {Link} from 'react-router-dom';
+import {Card, CardHeader, CardMedia, CardText, CardActions} from 'material-ui/Card';
+import {Button, Form, FormControl, FormGroup, Input} from 'react-bootstrap';
+import {MenuItem, Nav, Navbar, NavDropdown, NavItem, Row, Col} from 'react-bootstrap';
+import RaisedButton from 'material-ui/RaisedButton';
+import {IconButton, FlatButton} from 'material-ui';
+import '../components/auxiliary_position.css'
 
 const QuestionDetail = ({questionId, title, body, allowAddingAnswer}) => (
-    <div className="question-detail">
-      <div className="title">{title}</div>
-      <div className="body">{body}</div>
-      <Link to={allowAddingAnswer
-          ? `/questions/${questionId}/editAnswer`
-          : '/login'}>
-        <button>{allowAddingAnswer
-            ? 'Add Answer'
-            : 'Login to Add Answer'}</button>
-      </Link>
-    </div>
+    <Row className="col-md-10">
+      <Col xsOffset={4} className="col-centered" >
+      <Card style={{width: '100%', height: '30%'}}>
+        <CardHeader 
+          title={title}
+          actAsExpander={true}
+          showExpandableButton={true}/>
+        <CardText expandable={false }>
+            <div className="body">{body}</div>
+        </CardText>
+        <CardActions>
+          <Link to={allowAddingAnswer
+            ? `/questions/${questionId}/editAnswer`
+            : '/login'}>
+            <RaisedButton label={allowAddingAnswer
+              ? 'Add Answer'
+              : 'Login to Add Answer'} primary={true} linkButton={true}/>
+          </Link>
+        </CardActions>
+      </Card>
+      </Col>
+      <div style={{height:20}}/>
+    </Row>
+    
 );
 
 QuestionDetail.propTypes = {
@@ -24,3 +43,37 @@ QuestionDetail.propTypes = {
 };
 
 export default QuestionDetail;
+
+/*
+
+
+<Link to={allowAddingAnswer
+            ? `/questions/${questionId}/editAnswer`
+            : '/login'}>
+          <Button pullRight bsStyle="primary">{allowAddingAnswer
+              ? 'Add Answer'
+              : 'Login to Add Answer'}</Button>
+        </Link>
+
+
+<button>{allowAddingAnswer
+                ? 'Add Answer'
+                : 'Login to Add Answer'}</button>
+
+
+<div className='middle-row col-md-12' style={{width: '50%', margin: '0 auto'}}>
+    </div>
+
+
+<div className="question-detail">
+      <div className="title">{title}</div>
+      <div className="body">{body}</div>
+      <Link to={allowAddingAnswer
+          ? `/questions/${questionId}/editAnswer`
+          : '/login'}>
+        <button>{allowAddingAnswer
+            ? 'Add Answer'
+            : 'Login to Add Answer'}</button>
+      </Link>
+    </div>
+*/
